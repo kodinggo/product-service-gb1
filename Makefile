0e3222eb7449c3dadf1a5cd9fcd527e5ea9472c1
Frederich Blessy <frederichblessy@gmail.com>
2024-09-04 16:37:52 +0800

    implement graceful shutdown

--- Makefile
+++ Makefile
@@ -1,5 +1,2 @@
 run:
 	modd -f ./.modd/modd.conf
-
-grpc:
-	go run main.go grpcsrv
--- internal/console/http_server.go
+++ internal/console/http_server.go
@@ -1,12 +1,24 @@
 package console
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+
 	"github.com/go-playground/validator"
 	"github.com/kodinggo/product-service-gb1/db"
+	"github.com/kodinggo/product-service-gb1/internal/delivery/grpcservice"
 	"github.com/kodinggo/product-service-gb1/internal/delivery/http"
 	"github.com/kodinggo/product-service-gb1/internal/repository"
 	"github.com/kodinggo/product-service-gb1/internal/usecase"
 	"github.com/kodinggo/product-service-gb1/internal/utils"
+	pb "github.com/kodinggo/product-service-gb1/pb/product"
 	"github.com/labstack/echo/v4"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -62,8 +74,63 @@ func httpServer(cmd *cobra.Command, args []string) {
 
 	handler.Routes(e, authMiddleware.ValidateJWT)
 
-	err = e.Start(":3232")
+	s := grpc.NewServer()
+
+	svc := grpcservice.NewProductService(productUsecase)
+
+	pb.RegisterProductServiceServer(s, svc)
+
+	lis, err := net.Listen("tcp", ":8080")
 	continueOrFatal(err)
+
+	// initiate graceful shutdown
+	var wg sync.WaitGroup
+	errCh := make(chan error, 2)
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		if err := e.Start(":3232"); err != nil {
+			errCh <- err
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		log.Println("GRPC server is running on port :8080")
+
+		if err := s.Serve(lis); err != nil {
+			errCh <- err
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	select {
+	case <-quit:
+		log.Println("Shutting down servers...")
+
+		fmt.Println("anjsakjdnk")
+
+		// Gracefully shutdown the Echo server
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		if err := e.Shutdown(ctx); err != nil {
+			continueOrFatal(fmt.Errorf("Echo server shutdown failed: %v", err))
+		}
+
+		// Gracefully stop the gRPC server
+		s.GracefulStop()
+
+	case err := <-errCh:
+		continueOrFatal(err)
+	}
+
+	wg.Wait()
+	log.Println("Servers stopped gracefully")
 }
 
 func continueOrFatal(err error) {
